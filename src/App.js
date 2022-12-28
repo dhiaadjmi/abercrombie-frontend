226f7d800d49a9dc97daf5934c70eca388a61c0f
dhiaadjmi <ajmidhia7@gmail.com>
2022-12-28 18:59:10 +0100

    adding shippingScreen

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import SigninScreen from './screens/SigninScreen';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-
+import ShippingAddressScreen from './screens/ShippingAddressScreen';
 import { useContext } from 'react';
 import { Store } from './Store';
 
@@ -23,6 +23,7 @@ function App() {
   const signoutHandler = () => {
     ctxDispatch({ type: 'USER_SIGNOUT' });
     localStorage.removeItem('userInfo');
+    localStorage.removeItem('shippingAddress');
   };
   return (
     <BrowserRouter>
@@ -77,7 +78,10 @@ function App() {
               <Route path="/product/:slug" element={<ProductScreen />} />
               <Route path="/cart" element={<CartScreen />} />
               <Route path="/signin" element={<SigninScreen />} />
-
+              <Route
+                path="/shipping"
+                element={<ShippingAddressScreen />}
+              ></Route>
               <Route path="/" element={<HomeScreen />} />
             </Routes>
           </Container>
